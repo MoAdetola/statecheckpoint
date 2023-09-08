6c79e8888deec27b4f58b639fafbde1f3d086345
MoAdetola <phamosche7@gmail.com>
2023-09-08 10:01:37 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import image from './components/images/WhatsApp Image 2023-05-31 at 00.10.25.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Adetola',
+      bio: 'A passionate developer',
+      imgSrc: 'url-to-image',
+      profession: 'Web Developer'
+    },
+    show: false,
+    interval: 0
+  };
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({ interval: prevState.interval + 1 }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, show, interval } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {show && (
+          <div>
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={image} alt={person.fullName} width={300} height={300} />
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <p>Time interval since last component mount: {interval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
